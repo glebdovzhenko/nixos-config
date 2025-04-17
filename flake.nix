cb8f22308969595721f0d8837ad8e346566a64ba
Gleb Dovzhenko <dovjenko.g@gmail.com>
2025-04-17 13:05:03 +0700

    Trying to switch to neovim-nightly [3]

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.glebd = import ./home.nix;
-          nixpkgs.overlays = [inputs.neovim-nightly-overlay];
+          nixpkgs.overlays = [ inputs.neovim-nightly-overlay.overlays.default ];
         }
       ];
     };
