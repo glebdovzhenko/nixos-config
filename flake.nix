99d97bcf2f068e0e0a90025dbbd2c30e621994a3
Gleb Dovzhenko <dovjenko.g@gmail.com>
2025-04-17 14:16:07 +0700

    moved home.nix content to flake.nix

--- flake.nix
+++ flake.nix
@@ -17,10 +17,39 @@
         ./configuration.nix
         home-manager.nixosModules.home-manager
         {
+          nixpkgs.overlays = [ inputs.neovim-nightly-overlay.overlays.default ];
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.glebd = import ./home.nix;
-          nixpkgs.overlays = [ inputs.neovim-nightly-overlay.overlays.default ];
+          # glebd config
+          home-manager.users.glebd = {
+            home.username = "glebd";
+            home.homeDirectory = "/home/glebd";
+            home.stateVersion = "24.05";
+
+            programs.home-manager.enable = true;
+
+            programs.direnv = {
+              enable = true;
+              enableZshIntegration = true;
+              nix-direnv.enable = true;
+            };
+
+            programs.git = {
+              enable = true;
+              userName = "Gleb Dovzhenko";
+              userEmail = "dovjenko.g@gmail.com";
+            };
+
+            programs.neovim = {
+              enable = true;
+              viAlias = true;
+              vimAlias = true;
+              vimdiffAlias = true;
+            };
+
+            imports = [ ./programs ];
+          };
+
         }
       ];
     };
