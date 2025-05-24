606fbf384579dcb3188e91ca9a034a70649c9182
Gleb Dovzhenko <dovjenko.g@gmail.com>
2025-05-24 15:49:23 +0700

    Trying to store my own packages in nixos_config

---
+++ pkgs/xrt/default.nix
@@ -0,0 +1,22 @@
+{ 
+  pkgs,
+}:
+
+pkgs.python3.buildPythonPackage {
+  pname = "xrt";
+  version = "1.6.1";
+  src = pkgs.fetchFromGitHub {
+    owner = "kklmn";
+    repo = "xrt";
+    rev = "43a229b98c49669378e2e2127ba00ac9217de451";
+    sha256 = "sha256-BfMfAx/sN3Uf/JxY41DzDce0yAotFeI9o//wINeOVEM=";
+  };
+
+  postInstall = ''
+    makeWrapper $out/bin/xrtQookStart.py $out/bin/xrtQook --run "chmod -R 777 \$HOME/.xrt"
+    makeWrapper ${pkgs.python3.interpreter} $out/bin/xrtBentXtal \
+    --add-flags "$out/lib/python3.12/site-packages/xrt/gui/xrtBentXtal.py" \
+    --run "chmod -R 777 \$HOME/.xrt"
+  '';
+}
+
