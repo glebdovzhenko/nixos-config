2de5702b29bb243cf270139dd9bb163c6449af22
Gleb Dovzhenko <dovjenko.g@gmail.com>
2025-06-06 10:45:23 +0700

    learning nix lang the hard way [2]

--- pkgs/xrt/default.nix
+++ pkgs/xrt/default.nix
@@ -1,12 +1,13 @@
 { 
-  pkgs,
+  python3,
   buildPythonPackage,
+  fetchFromGitHub,
 }:
 
 buildPythonPackage {
   pname = "xrt";
   version = "1.6.1";
-  src = pkgs.fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "kklmn";
     repo = "xrt";
     rev = "43a229b98c49669378e2e2127ba00ac9217de451";
@@ -15,7 +16,7 @@ buildPythonPackage {
 
   postInstall = ''
     makeWrapper $out/bin/xrtQookStart.py $out/bin/xrtQook --run "chmod -R 777 \$HOME/.xrt"
-    makeWrapper ${pkgs.python3.interpreter} $out/bin/xrtBentXtal \
+    makeWrapper ${python3.interpreter} $out/bin/xrtBentXtal \
     --add-flags "$out/lib/python3.12/site-packages/xrt/gui/xrtBentXtal.py" \
     --run "chmod -R 777 \$HOME/.xrt"
   '';
