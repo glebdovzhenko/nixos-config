2c03c24a781ddbbbb33e516824d555a67d374706
Gleb Dovzhenko <dovjenko.g@gmail.com>
2025-06-06 13:09:10 +0700

    Working on gsas build scripts
    Working on gsas build scripts [2]
    
    Working on gsas build scripts [3]
    
    Working on gsas build scripts [4]
    
    Working on gsas build scripts [5]
    
    Working on gsas build scripts [6]
    
    Working on gsas build scripts [7]
    
    Working on gsas build scripts [8]
    
    Working on gsas build scripts [9]
    
    Working on gsas build scripts [10]
    
    Working on gsas build scripts [11]
    
    Working on gsas build scripts [12]

--- pkgs/gsas-ii/default.nix
+++ pkgs/gsas-ii/default.nix
@@ -1,35 +1,85 @@
-{ 
-  python,
-  buildPythonPackage,
-  fetchFromGitHub,
+{ pkgs
+, python
+, buildPythonPackage
+, fetchFromGitHub
+, openssl_3_4
+, gcc14
+, gfortran14
+, pkg-config
+, stdenv
+, numpy_1
+, scipy
+, meson
+, cython
+, pybind11
+, ninja
+, wxpython
+, matplotlib
+, pyopengl
+, pillow
+, h5py
+, hdf5plugin
+, requests
+, gitpython
+, sphinx
+, sphinx-rtd-theme
 }:
 
 buildPythonPackage {
   pname = "gsas-ii";
   version = "5.4.6";
-  format="other";
-  
+  format = "other";
+
   src = fetchFromGitHub {
     owner = "AdvancedPhotonSource";
     repo = "GSAS-II";
     tag = "v5.4.6";
     sha256 = "sha256-2ivan5S9BMEjJTZmvi223Lvb4BcuFPt/ZDS/H/32xF0=";
   };
-  
-  buildPhase=''
-  echo "BUILD PHASE"
-  runHook postBuild
+
+  dependencies = [
+    numpy_1
+    scipy
+    meson
+    cython
+    pybind11
+    ninja
+    wxpython
+    matplotlib
+    pyopengl
+    pillow
+    h5py
+    hdf5plugin
+    requests
+    gitpython
+    sphinx
+    sphinx-rtd-theme
+  ];
+
+  propagatedBuildInputs = [
+    openssl_3_4
+    gcc14
+    gfortran14
+    pkg-config
+    stdenv
+    python
+  ];
+
+  buildPhase = ''
+    runHook preBuild
+    echo "BUILD PHASE"
+    runHook postBuild
   '';
 
-  postBuild=''
+  installPhase = ''
+    runHook preInstall
+    echo "INSTALL PHASE"
+    mkdir -p $out/bin
+    makeWrapper ${python}/bin/python $out/bin/gsas-ii \
+      --add-flags "$src/GSASII/G2.py" \
+
+    runHook postInstall
   '';
 
-  installPhase=''
-  echo "INSTALL PHASE"
-  runHook postInstall
-  '';
-
-  postInstall = ''
-  '';
 }
 
