68ebd282746575c41d2ddcff8115adcbe75a5969
Gleb Dovzhenko <dovjenko.g@gmail.com>
2025-06-06 12:20:40 +0700

    Squash merge tests into main

--- pkgs/xrt/default.nix
+++ pkgs/xrt/default.nix
@@ -1,11 +1,13 @@
 { 
-  pkgs,
+  python,
+  buildPythonPackage,
+  fetchFromGitHub,
 }:
 
-pkgs.python3Packages.buildPythonPackage {
+buildPythonPackage {
   pname = "xrt";
   version = "1.6.1";
-  src = pkgs.fetchFromGitHub {
+  src = fetchFromGitHub {
     owner = "kklmn";
     repo = "xrt";
     rev = "43a229b98c49669378e2e2127ba00ac9217de451";
@@ -14,7 +16,7 @@ pkgs.python3Packages.buildPythonPackage {
 
   postInstall = ''
     makeWrapper $out/bin/xrtQookStart.py $out/bin/xrtQook --run "chmod -R 777 \$HOME/.xrt"
-    makeWrapper ${pkgs.python3.interpreter} $out/bin/xrtBentXtal \
+    makeWrapper ${python.interpreter} $out/bin/xrtBentXtal \
     --add-flags "$out/lib/python3.12/site-packages/xrt/gui/xrtBentXtal.py" \
     --run "chmod -R 777 \$HOME/.xrt"
   '';
